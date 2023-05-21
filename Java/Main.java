5b20a80b7e9b7c82ba6f13e9b2f52ae945afd6fb
Vishnu <talakokkula.vishnu2004@gmail.com>
2023-05-21 10:55:34 +0530

    changes

--- Java/Main.java
+++ Java/Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
 public class Main 
 {
     public static void main(String[] args) {
@@ -7,7 +9,6 @@ public class Main
         int second = sc.nextInt();
         String txt = sc.next();
         int age = sc.nextInt();
-        sc.close();
         System.out.println(first + second);
         System.out.println(first - second);
         System.out.println(first * second);
@@ -33,5 +34,38 @@ public class Main
         System.out.println(ch);
         System.out.println(a);
         System.out.println("Hello world");
+        // collections are framework in java
+        // lists 
+        ArrayList<String> al = new ArrayList<>();
+        al.add("kia");
+        al.add("MG");
+        al.add("hyundai");
+        int size = al.size();
+        al.set(1,"toyota");
+        al.remove(2);
+        System.out.println(al);
+        // maps
+       Map bikes = new HashMap();
+       bikes.put("Jupiter",2);
+       bikes.put("ktm",1);
+       System.out.println(bikes.get("ktm"));
+       System.out.println(bikes);
+       System.out.println("Welcome to the food app");
+       System.out.println("Create a password");
+       String pass1 = sc.next();
+       System.out.println("your password is: "+ pass1);
+       System.out.println("Enter your password");
+       String pass = sc.next();
+       while(!pass.equals(pass1)) {
+        System.out.println("wrong password. try again");
+        pass = sc.next();
+    }
+       System.out.println("granted");
+       System.out.println("Are you hungry, yes or no");
+       String ans = sc.next();
+       if(ans.equals("yes")){
+        
+       }
+
     }
 }
