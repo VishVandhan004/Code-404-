27f0f5e0aca98f330524c4ff692787d1c88d231b
Vishnu <talakokkula.vishnu2004@gmail.com>
2023-06-05 07:16:44 +0530

    changes

--- Java/Main.java
+++ Java/Main.java
@@ -2,15 +2,25 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the size of the array: ");
+        System.out.println("Enter the size of the array: ");
         int size = sc.nextInt();
-        System.out.println("enter the elements");
+        System.out.println("Enter the elements:");
         int arr[] = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        System.out.println(arr[arr.length - 1]);
+        boolean isSorted = true;
+        for (int i = 1; i < size; i++) {
+            if (arr[i] < arr[i - 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+        if (isSorted) {
+            System.out.println("The array is sorted.");
+        } else {
+            System.out.println("The array is not sorted.");
+        }
         sc.close();
     }
 }
