cca5da66a494405a7a56065f4153318223910baf
Vishnu <talakokkula.vishnu2004@gmail.com>
2023-07-04 20:24:44 +0530

    changes

--- Java/FDS/Main.java
+++ Java/FDS/Main.java
@@ -1,12 +1,26 @@
 import java.util.*;
-
 class Main {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the string: ");
-        String str = sc.nextLine();
-        if(str.contains("Vijay")){
-            str.replace("Vijay","Dinesh");
+        // HASHSET COLLECTION FRAMEWORK
+        HashSet<String> s = new HashSet<>();
+        // adding elements
+        s.add("Aashish");
+        s.add("Guna");
+        s.add("Mia");
+        s.add("Mia"); // redundant or duplicate data
+        // checking set size
+        int size = s.size();
+        System.out.println(size);
+        // check if element exists
+        boolean contains = s.contains("Mia");
+        System.out.println(contains);
+        // iterating over elements
+        for(String e: s){
+            System.out.println(e);
+        }
+        // remove element
+        s.remove("Mia");
+        // clear the whole set
+        s.clear();
         }
     }
-}
