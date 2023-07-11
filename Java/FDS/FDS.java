04f470020b9a703a543ce2b8d834f3cfa953315c
Vishnu <talakokkula.vishnu2004@gmail.com>
2023-07-11 21:35:06 +0530

    changes

--- Java/FDS/FDS.java
+++ Java/FDS/FDS.java
@@ -1,26 +1,33 @@
-import java.util.*;
+import java.io.*;
 class FDS {
-    public static void main(String args[]) {
-        // HASHSET COLLECTION FRAMEWORK
-        HashSet<String> s = new HashSet<>();
-        // adding elements
-        s.add("Aashish");
-        s.add("Guna");
-        s.add("Mia");
-        s.add("Mia"); // redundant or duplicate data
-        // checking set size
-        int size = s.size();
-        System.out.println(size);
-        // check if element exists
-        boolean contains = s.contains("Mia");
-        System.out.println(contains);
-        // iterating over elements
-        for (String e : s) {
-            System.out.println(e);
+    public static void main(String args[]) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Enter a file name: ");
+       FileOutputStream fos = new FileOutputStream("abc.txt");
+       fos.write(101);
+       fos.close();
+       String fname = br.readLine();
+       File f = new File(fname);
+       String result = f.exists() ? "exists" : "does not exist";
+       System.out.println("the given file: "+result);
+       if(f.exists()){
+        result = f.canRead() ? "readable" : "not readable";
+        System.out.println("the given file is"+result+f.getAbsolutePath());
+        result = f.canWrite() ? "writable" : "not writable";
+        System.out.println("the given file is:  "+result);
+        System.out.println("the given file length is "+f.length() + "in bytes");
+        if(fname.endsWith(".jpg") || fname.endsWith(".png")|| fname.endsWith(".gif")){
+            System.out.println("this is an image file");
         }
-        // remove element
-        s.remove("Mia");
-        // clear the whole set
-        s.clear();
+        else if(fname.endsWith(".exe")){
+            System.out.println("this is an executable file");
+        }
+        else if(fname.endsWith(".txt")){
+            System.out.println("this is a text file");
+        }
+        else {
+            System.out.println("Unknown file");
+        }
+       } 
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,18 +1,21 @@
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            FileInputStream file = new FileInputStream("abc.txt");
-            int size = file.available();
-            byte[] data = new byte[size];
-            file.read(data);
-            file.close();
-            String fileContent = new String(data);
-            System.out.println("Contents of the file:");
-            System.out.println(fileContent);
-        } catch (IOException e) {
-            System.out.println("Exception occurred: " + e.getMessage());
+            byte bwrite[] = {65,66,67,68,69};
+            OutputStream os = new FileOutputStream("test.txt");
+            for(int i=0;i<bwrite.length;i++){
+                os.write(bwrite[i]);
+            }
+            InputStream is = new FileInputStream("test.txt");
+            System.out.println("Available bytes in file:"+is.available());
+            int line;
+            while((line=is.read())!= -1){
+                System.out.println((char)line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
