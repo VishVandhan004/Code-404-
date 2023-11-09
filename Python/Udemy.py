ecdbd22a6e5bb49e2fed7138c2dc1c9fde9e0f32
Vishnu <talakokkula.vishnu2004@gmail.com>
2023-11-09 08:13:38 +0530

    changes

---
+++ Python/Udemy.py
@@ -0,0 +1,21 @@
+# String Slicing, Indexing and Methods
+ch = "ENGINEERING"
+print(ch[0])
+print(ch[-1])
+print(ch[4])
+print(ch[1:12:2])
+print(ch[2:])
+print(ch[:3])
+print(ch[3:6])
+print(ch.upper())
+
+#String Concatenation
+name = 'sam'
+ll = name[1:]
+print('p' + ll)
+print(name + '5')
+
+
+
+
+
