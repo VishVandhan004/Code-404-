218e37176a676a0cff49ced44e4f370a0b8371c1
Vishnu <talakokkula.vishnu2004@gmail.com>
2023-12-04 18:44:46 +0530

    changes

--- Python/Udemy.py
+++ Python/Udemy.py
@@ -1,5 +1,5 @@
 # String Slicing, Indexing and Methods
-ch = "ENGINEERING"
+ch = "ENGINEERING" 
 a = 'hello world'
 print(ch[0])
 print(ch[-1])
@@ -17,6 +17,10 @@ name = 'sam'
 ll = name[1:]
 print('p' + ll)
 print(name + '5')
+
+# Dictionaries
+my_dict = {'key1':'value1','key2':'values2'}
+print(my_dict['key1'])
 
 
 
