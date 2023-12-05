d05c5beb97d714e177310bde31a05858211f0ade
Vishnu <talakokkula.vishnu2004@gmail.com>
2023-12-05 22:10:53 +0530

    changes

---
+++ Artificial Intelligence/POS.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+def pos_tagging(text):
+    words = word_tokenize(text)
+    tagged_words = pos_tag(words)
+    return tagged_words
+
+# Example usage
+sentence = "Parts of speech tagging is an important NLP task."
+tags = pos_tagging(sentence)
+print(tags)
--- Python/Udemy.py
+++ Python/Udemy.py
@@ -21,6 +21,17 @@ print(name + '5')
 # Dictionaries
 my_dict = {'key1':'value1','key2':'values2'}
 print(my_dict['key1'])
+d = {'k1':123,'k2':[0,1,2],'k3':{'inside':100}}
+print(d['k2'])
+print(d['k2'][2])
+print(d['k3'])
+print(d['k3']['inside'])
+d['k4'] = 400 # adding a new element to the dictionary
+print(d)
+d1 = {'k1':['a','b','c']}
+print(d1['k1'])
+print(d1['k1'][2])
+
 
 
 
