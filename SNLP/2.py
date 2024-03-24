244d383eeaafdc709e31b1713c974b6aaf6d434c
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-03-24 23:26:17 +0530

    changes

--- SNLP/2.py
+++ SNLP/2.py
@@ -1,5 +1,5 @@
 str = input("Enter a string: ")
-# It is used to remove the spaces and return the new string
+# It is used to remove the spaces and return new string
 str = str.replace(" ","")
 print("REMOVING THE SPACES")
 print(str)
