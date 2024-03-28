3092c2a47921cea30f158271ea3c61e67f96b52c
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-03-28 22:37:24 +0530

    changes

--- SNLP/5.py
+++ SNLP/5.py
@@ -2,7 +2,6 @@ print("SENTENCE WITH NUMBERS AND STRINGS")
 def remove_numbers(sentence):
     sentence_without_numbers = ''.join(char for char in sentence if not char.isdigit())
     return sentence_without_numbers
-
 # Example usage
 original_sentence ="HI 1234,I am Studying in ngit btech 3rd year 5th sem."
 sentence_without_numbers = remove_numbers(original_sentence)
